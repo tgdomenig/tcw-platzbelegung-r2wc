d682e56b692318e3e1c2a69b90e03e084c8abcc6
“td” <tdomenig@it-couture.ch>
2023-05-30 13:13:20 +0200

    testing

--- src/components/Platzbelegung0.js
+++ src/components/Platzbelegung0.js
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react'
-import { Calendar, Views } from 'react-big-calendar'
+import React, {useState, useEffect} from 'react';
+import { Calendar, Views } from 'react-big-calendar';
 
-// import {Modal, Spin} from 'antd'
+// import {Modal, Spin} from 'antd';
 import {startOfWeek, addDays, format} from 'date-fns';
 
-import {localize, fmtEventStartEnd, formatTimestamp} from '../util/CalendarLocalizer'
-import {getToolbar} from '../util/CalendarToolbar'
+import {localize, fmtEventStartEnd, formatTimestamp} from '../util/CalendarLocalizer';
+import {getToolbar} from '../util/CalendarToolbar';
 
 
 //import {  Styled } from 'direflow-component';
 
-import calendarStyles from 'react-big-calendar/lib/css/react-big-calendar.css'
-import additionalStyles from './Platzbelegung.css'
+import calendarStyles from 'react-big-calendar/lib/css/react-big-calendar.css';
+import additionalStyles from './Platzbelegung.css';
 
-import {fetchEvents} from '../data/DataSource'
+import {fetchEvents} from '../data/DataSource';
 import { stageEvents } from '../data/StageEvents';
 
 export default function Platzbelegung() {
@@ -32,7 +32,7 @@ export default function Platzbelegung() {
 
   return(
     <div>
-      <div>This is a Test (8)</div>
+      <div>This is a Test (9)</div>
       <div>Just write anything here</div>
     </div>
   );
