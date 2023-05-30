f5adbdbeb81512e56ff83c4247265290331d6f66
“td” <tdomenig@it-couture.ch>
2023-05-30 17:09:28 +0200

    testing

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,10 +4,21 @@ export default function Test() {
 
   const [x, setX] = useState(0);
 
-  return(
-    <div>
-      <div>This is a Test (11)</div>
-      <div>Just write anything here</div>
-    </div>
-  );
+  if (x === 0) {
+    return(
+      <div>
+        <div>This is a Test [ZERO] (11)</div>
+        <div>Just write anything here</div>
+      </div>
+    );
+  }
+  else {
+    return(
+      <div>
+        <div>This is a Test [NOT ZERO] (11)</div>
+        <div>Just write anything here</div>
+      </div>
+    );
+
+  }
 }
