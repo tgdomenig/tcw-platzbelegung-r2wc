382d8efe63d23741047ee0492fa74bb9d2e7ff96
“td” <tdomenig@it-couture.ch>
2023-05-30 12:53:15 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import r2wc from '@r2wc/react-to-web-component';
 import Test from './components/Test.js';
 import Platzbelegung from './components/Platzbelegung.js';
 
-const testCmp = r2wc(Platzbelegung);
+const testCmp = r2wc(Test);
 
 customElements.define("my-platzbelegung-r2wc", testCmp);
