77a5d0ff1c0292b9f2f17042828d7cc6f4228691
“td” <tdomenig@it-couture.ch>
2023-05-30 13:27:30 +0200

    testing

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 
 export default function Test() {
+
+  const [x, setX] = useState(0);
+
   return(
     <div>
-      <div>This is a Test</div>
+      <div>This is a Test (11)</div>
       <div>Just write anything here</div>
     </div>
   );
