aa2c39054a813b353e2256ca183792b1ff786af7
“td” <tdomenig@it-couture.ch>
2023-05-30 13:07:14 +0200

    testing

--- src/components/Platzbelegung0.js
+++ src/components/Platzbelegung0.js
@@ -15,13 +15,24 @@ import additionalStyles from './Platzbelegung.css'
 
 import {fetchEvents} from '../data/DataSource'
 import { stageEvents } from '../data/StageEvents';
-/*
-*/
+
 export default function Platzbelegung() {
+
+  const [events, setEvents] = useState([])
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [targetRange, setTargetRange] = useState(null); // der Datumsbereich, der zumindest geladen werden soll
+  const [loadedRange, setLoadedRange] = useState(null); // der Datumsbereich, der effektiv geladen wurde
+  const [isLoading, setIsLoading] = useState(null); // Variable, um zu vermeiden, dass mehrere Ladevorgänge gleichzeitig asynchron getriggered werden
+
+  const [timestampDataLoaded, setTimestampDataLoaded] = useState(null);
+  const [eventPopupVisible, setEventPopupVisible] = useState(false);
+
 
   return(
     <div>
-      <div>This is a Test (6)</div>
+      <div>This is a Test (7)</div>
       <div>Just write anything here</div>
     </div>
   );
