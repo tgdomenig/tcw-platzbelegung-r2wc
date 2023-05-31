002504a5578ba2f7f865519474810a1589283038
“td” <tdomenig@it-couture.ch>
2023-05-31 16:16:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tcw-platzbelegung-r2wc/precache-manifest.c0fb0c066febeda36c4ce6e419cfe57a.js"
+  "/tcw-platzbelegung-r2wc/precache-manifest.8750cb01eaae4d78abdba29689b5231e.js"
 );
 
 workbox.clientsClaim();
